1c2c2eaa07b7897afbbbaaba8e10c4edefd50662
root <root@ThinkPadX230.localdomain>
2022-08-07 18:43:37 -0500

    Estilizar el detalle del producto y reparar los nombres de sus clases

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const desktopMenu= document.querySelector('.desktop-menu');
 const menuhamIcon= document.querySelector('.menu');
 const menucarritoIcon= document.querySelector('.navbar-shopping-cart');
 const mobileMenu= document.querySelector('.mobile-menu');
-const aside= document.querySelector('.product-detail');
+const shoppingCartContainer= document.querySelector('#shoppingCartContainer');
 const cardsContainer= document.querySelector('.cards-container');
 
 
@@ -12,17 +12,17 @@ menuhamIcon.addEventListener('click', toggleMobileMenu);
 menucarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu(){
-  const isAsideClosed= aside.classList.contains('inactive');
+  const isAsideClosed= shoppingCartContainer.classList.contains('inactive');
   if (!isAsideClosed){
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
   desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu(){
-  const isAsideClosed= aside.classList.contains('inactive');
+  const isAsideClosed= shoppingCartContainer.classList.contains('inactive');
   if (!isAsideClosed){
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
   }
   mobileMenu.classList.toggle('inactive');
 }
@@ -34,7 +34,7 @@ function toggleCarritoAside(){
     mobileMenu.classList.add('inactive');
     desktopMenu.classList.add('inactive');
   }
-  aside.classList.toggle('inactive');
+  shoppingCartContainer.classList.toggle('inactive');
 }
 
 const productList = [];
@@ -56,6 +56,7 @@ productList.push({
   image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 });
 
+// Esta función muestra la lista de productos con un For, podría usarse para mostrar otro array
 function renderProducts(arr) {
 for (product of arr){
   const productCard = document.createElement('div');
